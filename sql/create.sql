09a47400113537b9bcd11723813a047494dd95e3
Ramesh Pandey <104296742+009Rambo@users.noreply.github.com>
2024-02-02 03:16:29 +0200

    Delete sql directory

--- sql/create.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT,
-    city TEXT,
-    department INTEGER
-);
-
--- sql/insert.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO users (first_name, last_name, city, department)
-VALUES ('Rohan', 'Dutta', 'Kolkata', 20);
